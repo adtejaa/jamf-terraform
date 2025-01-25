fe7433e7c2d806405b91a1f1bb5a21ab787db208
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-25 22:33:06 +0530

    Update categories.tf

--- categories.tf
+++ categories.tf
@@ -1,7 +1,7 @@
 
- resource "jamfpro_category" "example_category_191" {
-   name     = "tf-example-category-0911"
-   priority = 1
- }
+ #resource "jamfpro_category" "example_category_191" {
+  # name     = "tf-example-category-0911"
+   #priority = 1
+ #}
 
 
