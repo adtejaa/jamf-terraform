36758462c630bfe5ff63d6a6353d2c44d255492a
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-25 19:16:07 +0530

    add git

--- categories.tf
+++ categories.tf
@@ -1,4 +1,4 @@
-
-resource "jamfpro_category" "example_category_13" {
-name     = "tf-example-category-032"
+resource "jamfpro_category" "example_category_3" {
+    "Testings"                = "1"
 }
+
