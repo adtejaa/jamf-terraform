dd8844d8f108f16d6a66476558b4cab46efeccc8
adtejaa <dharmatejapallapu2002@gmail.com>
2025-01-29 20:12:54 +0530

    added new building

--- jamfpro_building.tf
+++ jamfpro_building.tf
@@ -7,3 +7,13 @@ resource "jamfpro_building" "jamfpro_building_001" {
   zip_postal_code = "95014"
   country         = "The United States of America"
 }
+
+resource "jamfpro_building" "jamfpro_building_002" {
+  name            = "Apple Park-01"
+  street_address1 = "The McIntosh Tree"
+  street_address2 = "One Apple Park Way"
+  city            = "Cupertino"
+  state_province  = "Californiaa"
+  zip_postal_code = "950143"
+  country         = "The United States of America"
+}
