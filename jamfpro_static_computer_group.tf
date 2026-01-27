cd218374e0bcf337354fff7976ac03df7e4a409b
adtejaa <dharmatejapallapu2002@gmail.com>
2026-01-27 20:28:31 +0530

    Update jamfpro_static_computer_group.tf

--- jamfpro_static_computer_group.tf
+++ jamfpro_static_computer_group.tf
@@ -7,3 +7,14 @@ resource "jamfpro_static_computer_group" "managed_favourites_exclusion" {
     ]
   }
 }
+
+
+resource "jamfpro_static_computer_group" "managed_favourites_exclusion_new" {
+  name = "Managed Favourites Static new(tf)"
+
+  lifecycle {
+    ignore_changes = [
+      assigned_computer_ids
+    ]
+  }
+}
